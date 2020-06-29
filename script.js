2ce5aab238df3f13d1abb7c9dafa87b242782e73
nuvallo <nuvallo.inc@gmail.com>
2020-06-29 19:12:38 -0400

    storing price and movie seat to local storage

--- script.js
+++ script.js
@@ -6,9 +6,23 @@ const movieSelect = document.getElementById("movie");
 
 let ticketPrice = +movieSelect.value;
 
+// Save Selected Movie Index and Price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
 // Update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  // Copy selected seats into array
+  // Map through array
+  // Return a new array of indexes
+
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
 
   const selectedSeatsCount = selectedSeats.length;
 
@@ -19,6 +33,7 @@ function updateSelectedCount() {
 // Movie Select Event
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
+  setMovieData();
   updateSelectedCount();
 });
 
